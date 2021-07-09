eb38f466632849ea8d5d40a69b69c6796c3d7976
Duc Pham <phamminhduc0711@gmail.com>
2021-07-09 15:25:40 +0700

    Initial commit for counting vehicles

---
+++ count.py
@@ -0,0 +1,85 @@
+import argparse as ap
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+from tool.darknet2pytorch import Darknet
+from tool.utils import load_class_names, plot_boxes_cv2
+from tool.torch_utils import do_detect
+
+from sort import Sort
+
+
+def filter_objects(boxes):
+    # [bicycle, car, motorbike, bus, truck]
+    included_classes = [1, 2, 3, 5, 7]
+    keep = np.isin(boxes[:, 5].astype(np.int), included_classes)
+    return boxes[keep]
+
+
+if __name__ == "__main__":
+    parser = ap.ArgumentParser()
+    parser.add_argument("--video", type=str, required=True)
+    parser.add_argument("--save-output", action="store_true")
+    parser.add_argument("--cfg", type=str, default="./cfg/yolov4-tiny.cfg")
+    parser.add_argument("--weights", type=str, default="./weights/yolov4-tiny.weights")
+    parser.add_argument("--use-cuda", action="store_true")
+    args = parser.parse_args()
+
+    model = Darknet(args.cfg)
+    model.print_network()
+    model.load_weights(args.weights)
+    print(f'Loading weights from {args.weights}')
+    if args.use_cuda:
+        model.cuda()
+
+    num_classes = model.num_classes
+    if num_classes == 20:
+        namesfile = 'data/voc.names'
+    elif num_classes == 80:
+        namesfile = 'data/coco.names'
+    else:
+        namesfile = 'data/x.names'
+    class_names = load_class_names(namesfile)
+
+    tracker = Sort()
+    vid = cv2.VideoCapture(args.video)
+    num_frames = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+    pbar = tqdm(total=num_frames)
+
+    if args.save_output:
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        vw = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        vh = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        print("Video size", vw,vh)
+        out_video = cv2.VideoWriter(args.video.replace(".mp4", "_pred.mp4"),fourcc,20.0,(vw,vh))
+
+    while True:
+        ret, frame = vid.read()
+        sized = cv2.resize(frame, (model.width, model.height))
+        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+        boxes = do_detect(model, sized, 0.4, 0.6, use_cuda=args.use_cuda)[0]
+        boxes = filter_objects(np.array(boxes))
+        tracked_boxes = tracker.update(boxes)
+
+        res_img = plot_boxes_cv2(frame, tracked_boxes, class_names=class_names)
+        count_str = ", ".join(
+            [f"{class_names[k]}: {v}" for k, v in tracker.object_counts.items()]
+        )
+        res_img = cv2.putText(res_img, count_str, (15, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), 5)
+        res_img = cv2.putText(res_img, count_str, (15, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 255, 255), 2)
+
+        if args.save_output:
+            out_video.write(res_img)
+        else:
+            cv2.imshow('frame', res_img)
+            if cv2.waitKey(1) == ord('q'):
+                break
+            pbar.update(1)
+
+    pbar.close()
+    vid.release()
+    cv2.destroyAllWindows()
+    if args.save_output:
+        out_video.release()
--- tool/utils.py
+++ tool/utils.py
@@ -122,10 +122,11 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
             rgb = color
         else:
             rgb = (255, 0, 0)
-        if len(box) >= 7 and class_names:
-            cls_conf = box[5]
-            cls_id = box[6]
-            print('%s: %f' % (class_names[cls_id], cls_conf))
+        if len(box) >= 6 and class_names:
+            # cls_conf = box[4]
+            obj_id = int(box[4])
+            cls_id = int(box[5])
+            # print('%s: %f' % (class_names[cls_id], cls_conf))
             classes = len(class_names)
             offset = cls_id * 123457 % classes
             red = get_color(2, offset, classes)
@@ -133,8 +134,8 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
             blue = get_color(0, offset, classes)
             if color is None:
                 rgb = (red, green, blue)
-            img = cv2.putText(img, class_names[cls_id], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
-        img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 1)
+            img = cv2.putText(img, class_names[cls_id]+"-"+str(obj_id), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 2)
+        img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 2)
     if savename:
         print("save plot results to %s" % savename)
         cv2.imwrite(savename, img)
@@ -218,16 +219,16 @@ def post_processing(img, conf_thresh, nms_thresh, output):
                 ll_max_id = ll_max_id[keep]
 
                 for k in range(ll_box_array.shape[0]):
-                    bboxes.append([ll_box_array[k, 0], ll_box_array[k, 1], ll_box_array[k, 2], ll_box_array[k, 3], ll_max_conf[k], ll_max_conf[k], ll_max_id[k]])
+                    bboxes.append([ll_box_array[k, 0], ll_box_array[k, 1], ll_box_array[k, 2], ll_box_array[k, 3], ll_max_conf[k], ll_max_id[k]])
         
         bboxes_batch.append(bboxes)
 
     t3 = time.time()
 
-    print('-----------------------------------')
-    print('       max and argmax : %f' % (t2 - t1))
-    print('                  nms : %f' % (t3 - t2))
-    print('Post processing total : %f' % (t3 - t1))
-    print('-----------------------------------')
+#     print('-----------------------------------')
+#     print('       max and argmax : %f' % (t2 - t1))
+#     print('                  nms : %f' % (t3 - t2))
+#     print('Post processing total : %f' % (t3 - t1))
+#     print('-----------------------------------')
     
     return bboxes_batch
